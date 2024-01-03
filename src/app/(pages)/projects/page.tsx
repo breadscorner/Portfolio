201c1558e8f06cef1550c8f230af723f5b5304a2
Brett Gill <hello@brettgill.io>
2024-01-03 00:26:03 -0800

    update projects styling, prospective info, and json format

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,5 +1,15 @@
 import { Metadata } from "next";
 import projectsData from "../../../projectData/projectData.json";
+import Link from "next/link";
+
+const makeTextBeforeColonBold = (text: any) => {
+  const [beforeColon, afterColon] = text.split(":");
+  return (
+    <>
+      <strong>{beforeColon}</strong>: {afterColon}
+    </>
+  );
+};
 
 export const metadata: Metadata = {
   title: "Brett Gill | Projects",
@@ -10,19 +20,49 @@ export default function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-      {projectsData.map((project: any, index: any) => (
-        <section key={index}>
-          <h2>{project.title}</h2>
-          <p>{project.description}</p>
-          {project.technologies && (
-            <ul>
-              {project.technologies.map((tech: any, techIndex: any) => (
-                <li key={techIndex}>{tech}</li>
-              ))}
-            </ul>
-          )}
-        </section>
-      ))}
+      <div className="projects-container">
+        {projectsData.map((project: any, index: any) => (
+          <section key={index} className="project-section">
+            <Link href="https://vercel-prospective-three.vercel.app/">
+            <h2 className="project-title">
+              <strong>{project.title}</strong>
+            </h2>
+</Link>
+            {project.overview && (
+              <p className="project-overview">{project.overview}</p>
+            )}
+            {project.background && (
+              <p className="project-background">{project.background}</p>
+            )}
+
+            {project.features && (
+              <ul className="project-features">
+                {project.features.map((feature: any, featureIndex: any) => (
+                  <li key={featureIndex}>{makeTextBeforeColonBold(feature)}</li>
+                ))}
+              </ul>
+            )}
+
+            {project.technologies && (
+              <ul className="project-technologies">
+                {project.technologies.map((tech: any, techIndex: any) => (
+                  <li key={techIndex}>{makeTextBeforeColonBold(tech)}</li>
+                ))}
+              </ul>
+            )}
+
+            {project.getStarted && (
+              <ul className="project-get-started">
+                {project.getStarted.map((step: any, stepIndex: any) => (
+                  <li key={stepIndex}>{makeTextBeforeColonBold(step)}</li>
+                ))}
+              </ul>
+            )}
+
+            {project.note && <p className="project-note">{project.note}</p>}
+          </section>
+        ))}
+      </div>
     </div>
   );
 }
