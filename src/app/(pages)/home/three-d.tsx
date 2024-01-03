4aff62ed2cebe84a87c2befc56ab68f539d46941
Brett Gill <hello@brettgill.io>
2024-01-02 17:53:31 -0800

    move 3d component to new file in home so metadat works on home page

---
+++ src/app/(pages)/home/three-d.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import React, { useState, useCallback } from "react";
+
+interface ThreeDComponentProps {
+  children: React.ReactNode;
+}
+
+const ThreeDComponent: React.FC<ThreeDComponentProps> = ({ children }) => {
+  const [style3d, setStyle3d] = useState({
+    transform: "",
+    transformStyle: "preserve-3d" as React.CSSProperties["transformStyle"],
+  });
+
+  const handleMouseMove = useCallback((ev: any) => {
+    if (!ev.target) return;
+
+    const rect = ev.currentTarget.getBoundingClientRect();
+    const offsetX = ev.clientX - rect.left;
+    const offsetY = ev.clientY - rect.top;
+
+    // Check if the screen width is below a certain threshold
+    const isMobile = window.innerWidth <= 600;
+
+    // Adjust the rotation values based on the device type
+    const rotX = isMobile ? 0 : 90 - (offsetX / rect.width) * 180;
+    const rotY = isMobile ? 0 : 90 - (offsetY / rect.height) * 180;
+
+    setStyle3d({
+      transform: `rotateX(${rotY}deg) rotateY(${rotX}deg)`,
+      transformStyle: "preserve-3d",
+    });
+  }, []);
+
+  return (
+    <div onMouseMove={handleMouseMove}>
+      <div className="item-3d" style={style3d as React.CSSProperties}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ThreeDComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,31 @@
 // @ts-nocheck
 
-"use client";
-
 // React Imports
-import { useState, useCallback } from "react";
+import React from "react";
+import ThreeDComponent from "./(pages)/home/three-d";
+
+export const metadata: Metadata = {
+  title: "Brett Gill | Home",
+  description: "Full Stack Web Developer",
+};
 
 // Next Imports
-import Head from "next/head";
 import Image from "next/image";
 
-export default function HomePage() {
-  const [style3d, setStyle3d] = useState({
-    transform: "",
-    transformStyle: "preserve-3d" as React.CSSProperties["transformStyle"],
-  });
-
-  const handleMouseMove = useCallback((ev) => {
-    if (!ev.target) return;
-
-    const rect = ev.currentTarget.getBoundingClientRect();
-    const offsetX = ev.clientX - rect.left;
-    const offsetY = ev.clientY - rect.top;
-
-    // Check if the screen width is below a certain threshold
-    const isMobile = window.innerWidth <= 600;
-
-    // Adjust the rotation values based on the device type
-    const rotX = isMobile ? 0 : 90 - (offsetX / rect.width) * 180;
-    const rotY = isMobile ? 0 : 90 - (offsetY / rect.height) * 180;
-
-    setStyle3d({
-      transform: `rotateX(${rotY}deg) rotateY(${rotX}deg)`,
-      transformStyle: "preserve-3d",
-    });
-  }, []);
-
+export default function HomePage(): React.FC {
   return (
     <div>
-      <Head>
-        <title>Brett Gill | Home</title>
-        <meta name="description" content="Welcome to my portfolio" />
-      </Head>
       <div>
         <div>
-          <h1 className="">Brett Gill</h1>
-          <h2 className="">Full Stack Web Developer</h2>
+          <h1>Brett Gill</h1>
+          <h2>Full Stack Web Developer</h2>
         </div>
-        <div className="hidden md:block" onMouseMove={handleMouseMove}>
-          <div className="item-3d" style={style3d as React.CSSProperties}>
-            <div className="flex justify-center items-center">
-              <Image
-                src="/brett-logo.png"
-                alt="Brett Logo"
-                width={175}
-                height={175}
-              />
-            </div>
+        <ThreeDComponent>
+          <div className="flex justify-center items-center">
+            <Image src="/brett-logo.png" alt="Brett Logo" width={175} height={175} />
           </div>
-        </div>
+        </ThreeDComponent>
       </div>
     </div>
   );
-}
+};
