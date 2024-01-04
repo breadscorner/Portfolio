6c00780a578c8323fa981424525d7180c8f643b1
paarthd00 <paarthcode@gmail.com>
2024-01-03 18:40:05 -0800

    dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+"use client"
 import React from "react";
 
 import Footer from "./components/footer";
 import NavBar from "./components/navigation/navBar";
 import { menuItems } from "./components/navigation/navBar";
-
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 // Without this import, the Tailwind styles will not be applied
 import "./globals.css";
 
@@ -15,13 +17,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col container mx-auto mt-1 h-[100dvh]">
-      <NavBar />
-        <div className="flex-1">
-          {children}
-        </div>
-        <div className="mt-auto">
-          <Footer />
-        </div>
+        <NextUIProvider>
+          <NextThemesProvider attribute="class" defaultTheme="dark">
+            <div>
+              <NavBar />
+              <div className="flex-1 min-h-screen">
+                {children}
+              </div>
+              <div className="mt-auto">
+                <Footer />
+              </div>
+            </div>
+          </NextThemesProvider>
+        </NextUIProvider>
       </body>
     </html>
   );
