02bbddba06723620fefd6ba70be305cca3868ab9
Brett Gill <hello@brettgill.io>
2024-01-05 00:49:27 -0800

    add spinner logo using suspense for loading all pages

--- src/app/(pages)/_app.tsx
+++ src/app/(pages)/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from 'next/app';
 import CustomErrorComponent from '../components/error';
+import LoadingSpinner from '../components/loadingSpinner';
+import { Suspense } from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CustomErrorComponent>
-      {/* Your layout components or providers */}
-      <Component {...pageProps} />
+      <Suspense fallback={<LoadingSpinner />}>
+        {/* Your layout components or providers */}
+        <Component {...pageProps} />
+      </Suspense>
     </CustomErrorComponent>
   );
 }
---
+++ src/app/components/loadingSpinner.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from 'next/image';
+import "./globals.css";
+
+const LoadingSpinner: React.FC = () => {
+  return (
+    <div className="loading-spinner">
+      <Image 
+      src="/brett-logo-light.png" 
+      alt="Logo Spinner" 
+      width={100}
+      height={100}
+      className="spinner-image" />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
